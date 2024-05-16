329293da12bed61204c15b5d0d21f8c9cda31bfa
Michiel de Jong <michiel@pondersource.com>
2024-05-16 14:14:13 +0200

    Add Saiga source from ledgerloops-earthstar

---
+++ src/util.ts
@@ -0,0 +1,67 @@
+import { Message } from "./messages.ts";
+export { genRanHex } from "./genRanHex.ts";
+
+// returns a new object with the values at each key mapped using mapFn(value)
+export function objectMap(object, mapFn): object {
+  return Object.keys(object).reduce(function(result, key) {
+    result[key] = mapFn(object[key])
+    return result
+  }, {})
+}
+
+export class Entry {
+  sender: string;
+  receiver: string;
+  message: Message | string;
+  event: string;
+  constructor(sender: string, receiver: string, message: Message | string, event: string) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.message = message;
+    this.event = event;
+  }
+  describePath(): string {
+    if (this.event === 'sent') {
+      return `[${this.sender}]->[${this.receiver}]`;
+    } else {
+      return `[${this.sender}]>-[${this.receiver}]`;
+    }
+  }
+}
+function createPreamble(): string {
+  return `@startuml messages\n`;
+}
+function createLine(entry: Entry): string {
+  const colors = {
+    'probe': 'blue',
+    'trace': 'green',
+    'meet': 'orange',
+    'loop': 'red',
+    'have-probes': 'purple',
+    'okay-to-send-probes': 'purple',
+  };
+  if (entry.sender === '---') {
+    return '';
+  }
+  if (entry.event !== 'sent') {
+    return '';
+  }
+  if ((entry.message === 'have-probes') || (entry.message === 'okay-to-send-probes')) {
+    return '';
+  }
+  const parts = entry.message.toString().split(' ');
+  if (parts.length < 2) {
+    return '';
+  }
+  // if ((parts[0] !== 'meet') && (parts[1] !== 'genRanHex2')) {
+  //   return '';
+  // }
+  const color = colors[entry.message.toString().split(' ')[0]] || 'black';
+  return `${entry.sender} -[#${color}]-> ${entry.receiver}: ${entry.message}\n`;
+}
+function createEpilogue(): string {
+  return '@enduml';
+}
+export function createPlantUml(log: Entry[]): string {
+  return createPreamble() + log.map(line => createLine(line)).join('') + createEpilogue();
+}
